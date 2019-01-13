e4dd7325b933d50f1855e10a6c8bd654a433b576
samrishkor <samrishkor@gmail.com>
2019-01-13 22:53:50 +0300

    Added Repository file

---
+++ src/main/java/com/group/PostRepository.java
@@ -0,0 +1,11 @@
+package com.group;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.group.Post;
+
+
+
+public interface PostRepository extends CrudRepository<Post, Long>{
+
+}
